f43b58bacee0180adc7dbbed4430469c105c7348
Alberto <alup.gov@gmail.com>
2025-10-09 13:29:39 -0300

    desisto do fallback

--- index.ts
+++ index.ts
@@ -1,37 +1,38 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import express from 'express'
+import cors from 'cors'
 
-import routesTiposCursos from './routes/tiposcursos';
-import routesProfessores from './routes/professores';
-import routesCursos from './routes/cursos';
-import routesClientes from './routes/clientes';
-import routesLoginCli from './routes/loginClientes';
-import routesDuvidas from './routes/duvidas';
-import routesAdmins from './routes/admins';
-import routesLoginAdm from './routes/loginAdmins';
-import routesDashboard from './routes/dashboard';
+import 'dotenv/config'
 
-const app = express();
+import routesTiposCursos from './routes/tiposcursos'
+import routesProfessores from './routes/professores'
+import routesCursos from './routes/cursos'
+import routesClientes from './routes/clientes'
+import routesLoginCli from './routes/loginClientes'
+import routesDuvidas from './routes/duvidas'
+import routesAdmins from './routes/admins'
+import routesLoginAdm from './routes/loginAdmins'
+import routesDashboard from './routes/dashboard'
 
-app.use(express.json());
-app.use(cors());
+const app = express()
+const port = 3000
 
-// --- SUAS ROTAS DE API ---
-app.use("/tiposcursos", routesTiposCursos);
-app.use("/professores", routesProfessores);
-app.use("/cursos", routesCursos);
-app.use("/clientes", routesClientes);
-app.use("/clientes/login", routesLoginCli);
-app.use("/duvidas", routesDuvidas);
-app.use("/admins", routesAdmins);
-app.use("/admins/login", routesLoginAdm);
-app.use("/dashboard", routesDashboard);
+app.use(express.json())
+app.use(cors())
 
-// Rota principal para testar se a API está no ar
+app.use("/tiposcursos", routesTiposCursos)
+app.use("/professores", routesProfessores)
+app.use("/cursos", routesCursos)
+app.use("/clientes", routesClientes)
+app.use("/clientes/login", routesLoginCli)
+app.use("/duvidas", routesDuvidas)
+app.use("/admins", routesAdmins)
+app.use("/admins/login", routesLoginAdm)
+app.use("/dashboard", routesDashboard)
+
 app.get('/', (req, res) => {
-  res.send('API Orvion Cursos: Online');
-});
+  res.send('API: Cadastro de Cursos')
+})
 
-// --- ESSA LINHA É ESSENCIAL PARA A VERCEL ---
-export default app;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`)
+})
